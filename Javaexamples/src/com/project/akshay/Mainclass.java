5ee555236fc7dc12bc8741229719d9acb0bd2389
chede appliances <chede appliances@LAPTOP-IICJQSNP>
2020-10-02 16:57:19 +0530

    clear issue #1

--- Javaexamples/src/com/project/akshay/Mainclass.java
+++ Javaexamples/src/com/project/akshay/Mainclass.java
@@ -5,6 +5,7 @@ public class Mainclass {
 	public static void main(String[] args) {
 		System.out.println("hello...");
        System.out.println("by...");
+       System.out.println("clear issue...");
 	}
 
 }
