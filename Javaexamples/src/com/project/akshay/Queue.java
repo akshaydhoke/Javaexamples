b016badb7f5385b32cfb8636b0014cd59f8f62fd
chede appliances <chede appliances@LAPTOP-IICJQSNP>
2020-10-17 10:09:15 +0530

    add queue closes #2

--- Javaexamples/src/com/project/akshay/Queue.java
+++ Javaexamples/src/com/project/akshay/Queue.java
@@ -1,9 +1,17 @@
 package com.project.akshay;
 
+import java.util.*;
+
 public class Queue {
 
 	public static void main(String[] args) {
-		System.out.println("hiiii....");
+	 PriorityQueue<String> list=new PriorityQueue<String>();
+		list.add("aman");
+		list.add("sumit");
+		list.add("bhushan");
+		System.out.println(list.poll());
+		
+		
 	}
 
 }
